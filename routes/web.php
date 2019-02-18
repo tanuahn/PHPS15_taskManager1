0b53d8e123b91cdc0b5486f5fce48adaf6d0b2af
tanuahn <thieulatuananh92@gmail.com>
2019-02-18 10:35:07 +0700

    A

--- routes/web.php
+++ routes/web.php
@@ -1,33 +1,46 @@
 <?php
 
-Route::prefix('customer')->group(function () {
-    Route::get('index', function () {
-        // Hiển thị danh sách khách hàng
-        return view('modules.customer.index');
-    });
+//Route::prefix('customer')->group(function () {
+//    Route::get('index', function () {
+//        // Hiển thị danh sách khách hàng
+//        return view('modules.customer.index');
+//    });
+//
+//    Route::get('create', function () {
+//        // Hiển thị Form tạo khách hàng
+//    });
+//
+//    Route::post('store', function () {
+//        // Xử lý lưu dữ liệu tạo khách hàng thong qua phương thức POST từ form
+//    });
+//
+//    Route::get('{id}/show', function () {
+//        // Hiển thị thông tin chi tiết khách hàng có mã định danh id
+//    });
+//
+//    Route::get('{id}/edit', function () {
+//        // Hiển thị Form chỉnh sửa thông tin khách hàng
+//    });
+//
+//    Route::patch('{id}/update', function () {
+//        // xử lý lưu dữ liệu thông tin khách hàng được chỉnh sửa thông qua PATCH từ form
+//    });
+//
+//    Route::delete('{id}', function () {
+//        // Xóa thông tin dữ liệu khách hàng
+//    });
+//});
 
-    Route::get('create', function () {
-        // Hiển thị Form tạo khách hàng
-    });
-
-    Route::post('store', function () {
-        // Xử lý lưu dữ liệu tạo khách hàng thong qua phương thức POST từ form
-    });
-
-    Route::get('{id}/show', function () {
-        // Hiển thị thông tin chi tiết khách hàng có mã định danh id
-    });
-
-    Route::get('{id}/edit', function () {
-        // Hiển thị Form chỉnh sửa thông tin khách hàng
-    });
-
-    Route::patch('{id}/update', function () {
-        // xử lý lưu dữ liệu thông tin khách hàng được chỉnh sửa thông qua PATCH từ form
-    });
-
-    Route::delete('{id}', function () {
-        // Xóa thông tin dữ liệu khách hàng
-    });
+Route::get('/', function () {
+    return view('welcome');
 });
-
+// Tạo 1 nhóm route với tiền tố customer
+Route::prefix('customer')->group(function () {
+    Route::get('index', 'CustomerController@index');
+    Route::get('create', 'CustomerController@create');
+    Route::post('store', 'CustomerController@store');
+    Route::get('{id}/show', 'CustomerController@show');
+    Route::get('{id}/edit', 'CustomerController@edit');
+    Route::patch('{id}/update', 'CustomerController@update');
+    Route::delete('{id}', 'CustomerController@destroy');
+});
